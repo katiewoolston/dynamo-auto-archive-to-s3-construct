5b5a00fd0b5a8d7455264f82112c93f229e86309
Katie Woolston <katiewoolston@geniesolutions.com.au>
2022-04-14 10:44:28 +1000

    feat: release 0.0.1

--- .projenrc.js
+++ .projenrc.js
@@ -10,13 +10,22 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: PROJECT_NAME,
   repositoryUrl: 'https://github.com/katiewoolston/dynamo-auto-archive-to-s3-construct.git',
   description: 'A construct that automatically archives expired dynamo records to S3',
+  devDeps: [
+    'cdk',
+  ],
   deps: [
     'aws-cdk-lib',
     'constructs',
     '@aws-cdk/aws-apigatewayv2-alpha',
     '@aws-cdk/aws-apigatewayv2-integrations-alpha',
   ],
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  scripts: {
+    'cdk:deploy': "npx cdk deploy --require-approval never --app='./lib/integ.default.js'",
+    'cdk:destroy': "npx cdk destroy --app='./lib/integ.default.js'",
+  },
+  gitignore: [
+    'cdk.out',
+    '.projen',
+  ],
 });
 project.synth();
